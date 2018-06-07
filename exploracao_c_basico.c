5f52290c4913b0bc9e6cf419ecd5e70c8039eb8b
gabrielftrin <mrgabrielfernandes@hotmail.com>
2018-06-07 15:27:00 -0300

    Add files via upload

--- exploracao_c_basico.c
+++ exploracao_c_basico.c
@@ -1,1 +1,22 @@
+// Recursos básicos da linguagem C
+
+// Incluir no código a biblioteca (conjunto de funções pré-definidas) de funções básicas da liguagem.
 #include <stdio.h>
+
+// Criar a função principal 'main'
+int main () {
+
+	// Criar uma variavel, informa o tipo, depois o nome
+	int numero;
+
+	//Atribuir um valor a uma variável 
+	numero=10;
+
+	//Imprimir um texto na tela
+	printf("Este é um programa em C básico. \n");
+	// A expressão \n serve para o programa pular uma linha
+
+	//Imprimir o valor de uma variável, informa o tipo, depois o nome
+	printf("%d \n", numero);
+
+}
