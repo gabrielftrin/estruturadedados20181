42325c792b86eb3f8e452ef713b78949cdb3d876
gabrielftrin <mrgabrielfernandes@hotmail.com>
2018-06-07 18:23:37 -0300

    Add files via upload

--- base2.c
+++ base2.c
@@ -9,17 +9,17 @@ int main() {
 	//Estrutura 'if': Se a condição entre parenteses for verdadeira, executa o que estiver dentro das chaves, se não, pula essa parte.
 	if (1==1) {
 	//Usamos == para fazer comparações entre 2 elementos
-		numero=10;
+		numero=4;
 	}
 
 
 	//if else
 	//Se a condição não for verdadeira executa o que está dentro das chaves do else
-	if (numero=1) {
-		printf("O valor é 1");
+	if (numero==1) {
+		printf("O valor é 1. \n");
 	}
 	else {
-		printf("O valor não é 1");
+		printf("O valor não é 1. \n");
 	}
 
 
@@ -27,19 +27,23 @@ int main() {
 	//caso uma condição tenha determinado valor executa o que for programado para cada caso.
 	switch (numero)	{
 		case (10) : {
-			printf("Valor %d\n", numero);
+			printf("Valor: %d\n", numero);
 			break;
 		}
-		case 20 : {
-			printf("Valor %d\n", numero);
+		case (20) : {
+			printf("Valor: %d\n", numero);
 			break;
 		}
 		default : {
-			printf("O valor não e 10 nem 20. \n");
+			printf("O valor não é 10 nem 20. \n");
 		}
 	}
 
-
-
-
+	//Incondiiconal
+	//For: de um valor inicial até um valor final executa o que está dentro das chaves (separados por ; (ponto e virgula) )
+	printf("\nImpressão do For \n\n");
+	for (int i = 1; i < numero; ++i)
+	{
+		printf("Valor: %d\n", i);
+	}
 }
